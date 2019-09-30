67d32df1ed27a09570cc7cbc4722787c7562cf9c
JamesD101 <jamesodeyale01@gmail.com>
2019-09-30 05:14:22 +0100

    Added Redux

--- src/component/Headline/headline.test.js
+++ src/component/Headline/headline.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import Headline from "./index";
 
-import { findByTestAttribute, checkProps } from "./../../../Utils";
+import { findByTestAttribute, checkProps } from "../../../Utils";
 
 const setUp = (props = {}) => {
   const component = shallow(<Headline {...props} />);
